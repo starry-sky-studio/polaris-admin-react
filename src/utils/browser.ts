412b822a0972b1deeb2a64b573c4576e7cf66701
lvy <upwardsrwr@gmail.com>
2024-01-28 15:24:53 +0800

    feat: copy

--- src/utils/browser.ts
+++ src/utils/browser.ts
@@ -2,4 +2,23 @@ export class BrowserUtils {
   static openNewWindow(url: string) {
     window.open(url, '_blank')
   }
+
+  static copyText(text: string) {
+    if (navigator.clipboard) {
+      this.copyText = (text) => {
+        navigator.clipboard.writeText(text)
+      }
+      this.copyText(text)
+    } else {
+      this.copyText = (text) => {
+        const input = document.createElement('input')
+        input.setAttribute('value', text)
+        document.body.appendChild(input)
+        input.select()
+        document.execCommand('copy')
+        document.body.removeChild(input)
+      }
+      this.copyText(text)
+    }
+  }
 }
