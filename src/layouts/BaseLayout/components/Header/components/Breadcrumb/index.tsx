2e194068cfd92dff0b8ce7a110d6d447ca6aa8fc
lvy <upwardsrwr@gmail.com>
2024-02-10 21:02:03 +0800

    fix: bug

--- src/layouts/BaseLayout/components/Header/components/Breadcrumb/index.tsx
+++ src/layouts/BaseLayout/components/Header/components/Breadcrumb/index.tsx
@@ -30,10 +30,12 @@ const items = [
 export default function PoBreadcrumb() {
   function itemRender(item: any, params: any, items: any, paths: any) {
     console.log(item, 'item')
+    console.log(items, 'items')
     const last = items.indexOf(item) === items.length - 1
-    console.log(last, 'last')
+    //console.log(last, 'last')
     return last ? <span>{item.title}</span> : <Link to={paths.join('/')}>{item.title}</Link>
   }
+
   return (
     <Breadcrumb
       itemRender={itemRender}
